026006e57649c5307d041ddd687765f6b96ed7fe
Reinmand Trial <max.gluschtsov@gmail.com>
2021-06-02 22:25:24 +0300

    valw3

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,58 @@
-
-
-new Swiper('.swiper-container', {
-    // навигация, кнопки
-    navigation: {
-        nextEl: '.products-slider__button_next',
-        prevEl: '.products-slider__button_prev',
-    },
-    // модификации 
-    grabCursor: true, //  курсор-рука
-    autoHeight: true, // автовысота
-    slidesPerView: 'auto', //кол-во показываемых слайдов
-    watchOverflow: true, //если слайдов мало, - слайдер отключается
-    spaceBetween: 20, // отступы между слайдами
-    loop: true, // бесконечный слайдер
-    slideToClickedSlide: false,
-    breakpoints: {
-        0: {
-            spaceBetween: 4,
+document.querySelectorAll('.swiper-container').forEach(function (elem) {
+    new Swiper(elem, {
+        navigation: {
+            nextEl: elem.nextElementSibling.nextElementSibling,
+            prevEl: elem.nextElementSibling,
         },
-        768: {
-            spaceBetween: 20,
-            slidesPerView: '4',
+        grabCursor: true, //  курсор-рука
+        autoHeight: true, // автовысота
+        slidesPerView: 'auto', //кол-во показываемых слайдов
+        watchOverflow: true, //если слайдов мало, - слайдер отключается
+        spaceBetween: 20, // отступы между слайдами
+        loop: true, // бесконечный слайдер
+        slideToClickedSlide: false,
+        breakpoints: {
+            0: {
+                spaceBetween: 4,
+            },
+            991: {
+                spaceBetween: 20,
+                slidesPerView: '4',
+            },
+            1200: {
+                slidesPerView: '5',
+            },
         },
-        1200: {
-            slidesPerView: '5',
-        },
-    },
+    });
 });
+
+// new Swiper('.swiper-container', {
+//     // навигация, кнопки
+//     navigation: {
+//         nextEl: '.products-slider__button_next',
+//         prevEl: '.products-slider__button_prev',
+//     },
+//     // модификации 
+//     grabCursor: true, //  курсор-рука
+//     autoHeight: true, // автовысота
+//     slidesPerView: 'auto', //кол-во показываемых слайдов
+//     watchOverflow: true, //если слайдов мало, - слайдер отключается
+//     spaceBetween: 20, // отступы между слайдами
+//     loop: true, // бесконечный слайдер
+//     slideToClickedSlide: false,
+//     breakpoints: {
+//         0: {
+//             spaceBetween: 4,
+//         },
+//         768: {
+//             spaceBetween: 20,
+//             slidesPerView: '4',
+//         },
+//         1200: {
+//             slidesPerView: '5',
+//         },
+//     },
+// });
 new Swiper('.swiper-banner', {
     watchOverflow: true, //если слайдов мало, - слайдер отключается
     loop: true, // бесконечный слайдер
@@ -110,31 +136,84 @@ $('.btn-login').on('click', function () {
     $('body').removeClass('burger-open');
 })
 // Войти попап конец
-$(window).on("load resize",(function(){
-      if($(window).width()<"576"){
+$(window).on("load resize", (function () {
+    if ($(window).width() < "576") {
         $('.header__nav-right').append($('.burger-search'));
-      }else{
-          $('.burger-table__list-mobile').append($('.burger-search'));
-      }
-  }));
-  //mobile burger search
-$('.btn-search-mobile').on('click',function(){
+    } else {
+        $('.burger-table__list-mobile').append($('.burger-search'));
+    }
+}));
+//mobile burger search
+$('.btn-search-mobile').on('click', function () {
     $('.header__nav-right').addClass('open');
     $('.logo').toggleClass('d-none')
 })
-$(document).on('click', function(e) {
+$(document).on('click', function (e) {
     if (!$(e.target).closest(".burger-search").length && !$(e.target).closest(".btn-search-mobile").length) {
-      $('.logo').removeClass('d-none');
-      $('.header__nav-right').removeClass('open');
+        $('.logo').removeClass('d-none');
+        $('.header__nav-right').removeClass('open');
     }
     e.stopPropagation();
-  });
-  //end
+});
+//end
 
-  // add to favorites
-$('.btn-heart').on('click',function() {
+// add to favorites
+$('.btn-heart').on('click', function () {
     $(this).find('.icon-heart').toggleClass('active');
 })
-  // add to favorit
+// add to favorit
 
-  
+// Выбор размера
+$('.basket-item__size-header').on('click', function () {
+    $(this).toggleClass('open');
+    if ($(this).hasClass('open')) {
+        $(this).next().fadeIn();
+    } else {
+        $(this).next().fadeOut();
+    }
+})
+// $('.basket-item__size-item').on('.clock')
+//Выбор размера коанец
+
+// amount 
+$('.amount-plus').on('click', function () {
+    var btn = $(this);
+    var body = btn.closest('.amount');
+    var count = body.find('.amount-count');
+    var kol = parseInt(count.text());
+    count.text(kol + 1);
+})
+$('.amount-minus').on('click', function () {
+    var btn = $(this);
+    var body = btn.closest('.amount');
+    var count = body.find('.amount-count');
+    var kol = parseInt(count.text());
+    if (kol > 0) {
+        count.text(kol - 1);
+    }
+})
+// amount end
+
+$('.page-basket__promo-title').on('click', function () {
+    $(this).toggleClass('open');
+    if ($(this).hasClass('open')) {
+        $(this).next().fadeIn();
+
+    } else {
+        $(this).next().fadeOut();
+    }
+})
+
+$('.form-order__radio-input').on('click', function () {
+    var items = $(this).closest('.form-order__radio-container').find('.form-order__radio-box');
+    $(items).each(function () {
+        $(this).removeClass('open')
+    })
+    $(this).closest('.form-order__radio-box').addClass('open');
+})
+
+$(function () {
+    $('body').on('input', '.input-number', function () {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    });
+})
